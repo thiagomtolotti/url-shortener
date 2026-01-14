9f059c48aefbf939b0ea7ae5500bbf7355eacbd3
Thiago Tolotti <thiago.tolotti@riole.com.br>
2026-01-14 11:38:07 -0300

    initially adds createURL request

--- main.go
+++ main.go
@@ -8,13 +8,39 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", ping)
+	mux.HandleFunc("/create", createURL)
 
 	http.ListenAndServe(":3001", mux)
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+	createMessageResponse(w, http.StatusOK, "Service is online")
+}
+
+type CreateRequest struct {
+	URL string `json:"url"`
+}
+
+func createURL(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		createMessageResponse(w, http.StatusMethodNotAllowed, "Method not allowed")
+		return
+	}
+
+	var req CreateRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		createMessageResponse(w, http.StatusBadRequest, "Invalid request")
+		return
+	}
+
+	createMessageResponse(w, http.StatusCreated, "URL was created successfully")
+}
+
+func createMessageResponse(w http.ResponseWriter, status int, message string) {
+	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Service is online",
+		"message": message,
 	})
 }
